90802ea114bf8e86fbf56fb752cefbfab8304968
catso10 <sahilbissessur@umail.ucsb.edu>
2015-11-27 22:56:56 +0000

    add initial jquery

---
+++ app/assets/javascripts/scripts.js
@@ -0,0 +1,7 @@
+// making sure the document is loaded before using jQuery
+$("document").ready(function(){
+
+// this is called content creation
+$("body").append("<h1>This is a heading</h1>");
+
+});

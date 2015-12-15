fd956cef0ea041d9cb3265ab69e2d71f1a1aab7c
Styxx <styxx_n_stones@live.com>
2015-12-15 05:24:31 +0000

    Remove pinging js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,9 +21,3 @@
 //        $("#msgList").load('/users/9/messages #msgList');
 //    });
 //});
-
-
-var http = require("http");
-setInterval(function() {
-    http.get("http://<your app name>.herokuapp.com");
-}, 300000); // every 5 minutes (300000)

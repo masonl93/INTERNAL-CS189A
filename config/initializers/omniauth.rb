14e27cf647d80ea035badbcfb90b44b4adfb9434
Prithvi Sathiya <prithvisathiya@169-231-112-22.wireless.ucsb.edu>
2016-01-12 23:37:47 -0800

    backup

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  # provider :facebook, '721955464606146', '9b0e2bd86dc652a59ce90eb29effbc55'
-  provider :facebook, '999266156778410', 'acfe9ca3316d262cbc32053b2e5f90b4'
+  provider :facebook, '721955464606146', '9b0e2bd86dc652a59ce90eb29effbc55'
+  #provider :facebook, '721955464606146', 'acfe9ca3316d262cbc3dfdfdfdfdfd2053b2e5f90b4'
 end

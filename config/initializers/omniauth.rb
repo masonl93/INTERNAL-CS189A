7900b70c32f4794f39c9629633de9f99772e0ee6
Styxx <styxx_n_stones@live.com>
2016-01-27 02:04:41 +0000

    Fix db

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  # provider :facebook, '536931563143013', '9ed50ecce3c324998dc71d996e35c572'
-  provider :facebook, '999266156778410', 'acfe9ca3316d262cbc32053b2e5f90b4'
+  provider :facebook, '536931563143013', '9ed50ecce3c324998dc71d996e35c572'
 end

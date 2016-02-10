171392e8bb708affa7400f855b290510abb45308
sbissessur <sahilbissessur@gmail.com>
2016-02-10 01:39:34 +0000

    My omniauth passwordgit status!

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '721955464606146', '9b0e2bd86dc652a59ce90eb29effbc55'
+  provider :facebook, '999266156778410', 'acfe9ca3316d262cbc32053b2e5f90b4'
 end

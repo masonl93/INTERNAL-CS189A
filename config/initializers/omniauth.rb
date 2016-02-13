27cb41bc5afff636879476a67efd7794f9ab584f
Styxx <styxx_n_stones@live.com>
2016-02-13 20:17:43 +0000

    App button logic

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '1705066526395154', 'ad995d0d6e2193cc4fa7d1190def6011'
+  provider :facebook, '536931563143013', '9ed50ecce3c324998dc71d996e35c572'
 end
